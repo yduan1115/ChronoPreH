e6a6d83a0f5961aa015c3caa044244caa3411503
Y.D. <yduan@bjmu.edu.cn>
2025-05-20 16:38:58 +0800

    Add files via upload

---
+++ tests/testthat/FitCat_test.R
@@ -0,0 +1,26 @@
+library(nnet)         # multinom
+library(mgcv)         # gam
+library(randomForest) # randomForest
+library(ranger)       # ranger
+library(e1071)        # svm
+library(caret)
+
+source("/Users/Yun/Desktop/R_predM/ChronoPreH/R/FitCat.R")
+
+time <- 1:10
+class <- c("A", "A", "B", "B", "B", "A", "A", "B", "B", "A")
+future_time <- 12
+true_class <- "B"
+
+set.seed(123)
+result <- FitCat(time, class, future_time, true_class)
+
+print(result$best_model) # svm
+print(result$probabilities)
+print(result$predictions)
+
+source("/Users/Yun/Desktop/R_predM/ChronoPreH/R/FitCat_vi.R")
+fit_result = result
+FitCat_vi(fit_result,true_class)
+
+
